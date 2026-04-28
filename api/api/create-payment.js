b4088d86485f4fcb7bd30525b4c2eee99b7507e9
rachantra196-del <rachantra196@gmail.com>
2026-04-28 16:41:40 +0700

    Create create-payment.js

---
+++ api/api/create-payment.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const { amount } = req.body || {};
+
+  res.status(200).json({
+    success: true,
+    data: {
+      payment_link: "https://example.com/pay?amount=" + amount
+    }
+  });
+}
