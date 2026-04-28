f819a50c416d6d45dd6388a65f623960317b94b7
rachantra196-del <rachantra196@gmail.com>
2026-04-28 16:24:56 +0700

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,21 +1,4 @@
-async function pay() {
-  console.log("BUTTON CLICKED");
-
+function pay() {
   const amount = document.getElementById("amount").value;
-
-  console.log("AMOUNT ENTERED:", amount);
-
-  const res = await fetch("/api/create-payment", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ amount })
-  });
-
-  const data = await res.json();
-
-  console.log("API RESPONSE:", data);
-
-  alert("Check console (F12)");
+  alert(amount);
 }
