03f89553f61bf9ad247b2b941211d2606a72c716
rachantra196-del <rachantra196@gmail.com>
2026-04-29 14:45:37 +0700

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,1 +1,57 @@
 
+import axios from "axios";
+import crypto from "crypto";
+
+function sign(params, secret) {
+  const sorted = Object.keys(params)
+    .sort()
+    .map(k => `${k}=${params[k]}`)
+    .join("&");
+
+  return crypto.createHash("md5").update(sorted + secret).digest("hex");
+}
+
+async function getToken() {
+  const res = await axios.post(process.env.TOKEN_URL, {
+    grant_type: "password",
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+  });
+
+  return res.data.access_token;
+}
+
+export default async function handler(req, res) {
+  try {
+    const TOKEN = await getToken();
+
+    const payload = {
+      service: "webpay.acquire.queryOrder",
+      sign_type: "MD5",
+      out_trade_no: req.query.id
+    };
+
+    payload.sign = sign(payload, process.env.SECRET_KEY);
+
+    const response = await axios.post(
+      process.env.GATEWAY_URL,
+      payload,
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`
+        }
+      }
+    );
+
+    res.status(200).json({
+      status: response.data.trade_status
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.response?.data || err.message
+    });
+  }
+}
